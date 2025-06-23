878ee0d5d50046184fc7fb95493dafb368c9d95b
Paddy9814 <pattym98@gmx.de>
2025-06-23 18:19:29 +0200

    Update player.js

--- player.js
+++ player.js
@@ -7,7 +7,7 @@ const shellCountPerColorDisplay = document.getElementById('shell-count-per-color
 const colorNameDisplay = document.getElementById('color-name');
 const colorSelectionContainer = document.getElementById('color-selection');
 
-const colors = ['pink', 'black', 'lightblue', 'darkblue', 'white', 'peachpuff', 'plum', 'indigo'];
+const colors = ['pink', 'black', 'lightblue', 'darkblue', 'white'];
 const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 const backgroundColor = '#faedcd';
